50a5fddc3340642d6f0761ae54f5db815b0f9a9d
Rishi Manu <manur@tcd.ie>
2024-02-02 16:26:07 +0000

    Implement backend API
    README.md: API reference.
    Dockerfile,compose.yaml: Cotainerise the backend.
    app.py: The Flask application providing the API gateway.

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app = Flask(__name__)
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+       uploaded_file.save(uploaded_file.filename)
+    return { "video-id": "ef1d3c7b-257c-4d5e-9f61-38750a1e06d1" }
+
+
+@app.route("/frames/<uuid:video_id>")
+def frames(video_id):
+    return { "frames": [] }
+
+
+@app.route("/results/<uuid:video_id>")
+def results(video_id):
+    return { "total-score": [], "frame-scores": [] }
+
