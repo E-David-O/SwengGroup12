61f31e0b2ab72ab6dd2cf42daae29456f5f90ae8
Ayushmaan04 <yadavay@tcd.ie>
2024-02-21 17:10:08 +0000

    playing with yolo results

--- FrameAnanlysis/yolo_analysis.py
+++ FrameAnanlysis/yolo_analysis.py
@@ -1,5 +1,15 @@
 from ultralytics import YOLO
-
+frame = "FrameAnanlysis\\testImg.jpeg"
 model = YOLO('yolov8n.pt')
 
-results = model(source="FrameAnanlysis\\test01.mp4", show = True, conf = 0.4, save = True)
+def frameAnalysis(frame):
+    results = model(source= frame, show = False, conf = 0.4, save = True)
+    print(results)
+
+
+    # View results
+    for r in results:
+        print(r.boxes)  # print the Boxes object containing the detection bounding boxes
+
+
+frameAnalysis(frame)
