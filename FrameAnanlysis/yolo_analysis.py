11563670aeaf41c6062f540c323c9c5b99ffdf99
Ayushmaan04 <yadavay@tcd.ie>
2024-02-27 11:41:49 +0000

    test

--- FrameAnanlysis/yolo_analysis.py
+++ FrameAnanlysis/yolo_analysis.py
@@ -4,12 +4,9 @@ model = YOLO('yolov8n.pt')
 
 def frameAnalysis(frame):
     results = model(source= frame, show = False, conf = 0.4, save = True)
-    print(results)
 
 
     # View results
     for r in results:
-        print(r.boxes)  # print the Boxes object containing the detection bounding boxes
-
-
+        print(r.keypoints)
 frameAnalysis(frame)
