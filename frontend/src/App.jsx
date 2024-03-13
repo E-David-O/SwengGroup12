894a0225952a5f86abdcd627c1a9fced965edd97
Finn Cummins <finnkcummins@gmail.com>
2024-03-13 16:17:49 +0000

    Re-formatted Object Accuracy object

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,11 +15,11 @@ function App() {
           <Route path="*" element={<VideoUpload />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/objectaccuracy" element={
-  <ObjectAccuracy objectAccuracyData={[
-    { object: 'Cat', accuracy: 98.5 },
-    { object: 'Dog', accuracy: 89.7 }
-  ]}/>
-}/>
+            <ObjectAccuracy objectAccuracyData={[
+              { object: 'Cat', accuracy: 98.5 },
+              { object: 'Dog', accuracy: 89.7 }
+            ]}/>
+          }/>
 
           <Route path="/video" element={<VideoUpload />} />
           <Route path="/live" element={<LiveVideo />} />
