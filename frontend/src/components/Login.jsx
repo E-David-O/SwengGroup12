83b2367e9249120af39cf70548a42c647bc51e6d
Finn Cummins <finnkcummins@gmail.com>
2024-03-15 13:54:03 +0000

    Changed password input element to type password

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -57,7 +57,8 @@ export default function Login() {
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                             id="inputPassword" 
                              name="password" 
-                             placeholder="••••••••"
+                             placeholder="Enter Password"
+                             type="password"
                              onChange={(e) => setPassword(e.target.value)}
                             ></input>
                         </div>
