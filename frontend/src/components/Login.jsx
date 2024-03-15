a5579d227976b12e8070b5495ab631200bf24e6d
Finn Cummins <finnkcummins@gmail.com>
2024-03-15 13:36:49 +0000

    Changed the Sign in button so that it is now visible and has a blue background

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -61,7 +61,12 @@ export default function Login() {
                              onChange={(e) => setPassword(e.target.value)}
                             ></input>
                         </div>
-                        <button onClick={(e) => handleSubmit(e)}id="submit-button" type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                        <button onClick={(e) => handleSubmit(e)} id="submit-button" type="submit" 
+                                className="mt-4 w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 
+                                focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium 
+                                rounded-lg text-sm px-5 py-2.5 text-center transition duration-150 ease-in-out">
+                            Sign in
+                        </button>
                        </form>
                     
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
