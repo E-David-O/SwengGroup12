7afd73812a4364ba98094aaffb7e183411104dcd
Finn Cummins <finnkcummins@gmail.com>
2024-03-16 15:50:32 +0000

    Updated registration button

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -58,11 +58,17 @@ export default function Login() {
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                             id="inputPassword" 
                              name="password" 
-                             placeholder="••••••••"
+                             type="password"
+                             placeholder="Enter Password"
                              onChange={(e) => setPassword(e.target.value)}
                             ></input>
                         </div>
-                        <button onClick={(e) => handleSubmit(e)}id="submit-button" type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                        <button onClick={(e) => handleSubmit(e)} id="submit-button" type="submit" 
+                                className="mt-4 w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 
+                                focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium 
+                                rounded-lg text-sm px-5 py-2.5 text-center transition duration-150 ease-in-out">
+                            Register
+                        </button>
                        </form>
                     
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
