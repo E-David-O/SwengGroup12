e9178d3b4d0bf1b5268ccb36fc84d158b3fdc9cd
Finn Cummins <finnkcummins@gmail.com>
2024-03-17 12:00:03 +0000

    Changed default page to the landing page. User is re-directed to video upload page upon log-in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,27 @@
 import { Suspense } from 'react'
 import './App.css'
-import VideoUpload from './components/VideoUpload'
+import HomePage from './components/HomePage';
+import VideoUpload from './components/VideoUpload';
 import { Route, Routes } from "react-router-dom";
 import LiveVideo from './components/LiveVideo';
 import VideoAnalysis from './components/VideoAnalysis';
+import { VideoContext } from "./components/VideoUtil";
+import { useContext } from "react";
 import Login from './components/Login';
 import Signup from './components/Signup';
+
 function App() {
+  const { token, deleteData, logout } = useContext(VideoContext);
   
   return (
     <>
       <Suspense fallback={null}>
         <Routes>
-          <Route path="*" element={<VideoUpload />} />
+          { token == "" ? 
+              <Route path="*" element={<HomePage />} />
+              : 
+              <Route path="*" element={<VideoUpload />} />
+          }
           <Route path="/video" element={<VideoUpload />} />
           <Route path="/live" element={<LiveVideo />} />
           <Route path="/analysis/:id" element={<VideoAnalysis />}/>
