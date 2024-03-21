3b450627d462f466a4edee6a06a2561ea9f24129
Finn Cummins <finnkcummins@gmail.com>
2024-03-21 20:17:08 +0000

    Added names to the about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -13,8 +13,33 @@ function About() {
     return (
         <>
             <Navbar />
-            <h1 className="text-center text-3xl font-bold py-4">SWENG Group 12</h1>
-            <Footer />
+            <div className="text-center">
+                <h1 className="text-3xl font-bold py-4">SWENG Group 12</h1>
+                <h2 className="text-2xl font-bold py-4">The frontend team</h2>
+                <ul>
+                    <li>Louie Somers</li>
+                    <li>Finn Cummins</li>
+                    <li>Afaf Shadani</li>
+                    <li>Daire Frankling</li>
+                </ul>
+                <h2 className="text-2xl font-bold py-4">The Database team</h2>
+                <ul>
+                    <li>Eimhin Heenan-Roberts</li>
+                    <li>Conor Daly</li>
+                </ul>
+                <h2 className="text-2xl font-bold py-4">API team</h2>
+                <ul>
+                    <li>Ming Him Foun</li>
+                    <li>Nicolas Moschenross</li>
+                    <li>Rishi Manu</li>
+                </ul>
+                <h2 className="text-2xl font-bold py-4">Frame analysis team</h2>
+                <ul>
+                    <li>Emeka David Odoemelam</li>
+                    <li>Ayushmaan Kumar Yadav</li>
+                </ul>
+                <Footer />
+            </div>
         </>
     );
 }
