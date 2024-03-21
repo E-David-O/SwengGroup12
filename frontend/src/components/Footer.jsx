67bf18e6b4ff97094c3de7ffbe61c2efa477f380
Finn Cummins <finnkcummins@gmail.com>
2024-03-21 12:50:07 +0000

    Added username to the home dashboard and implemented colour transition feature

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -16,14 +16,13 @@ function Footer() {
     return (
         <footer className="sticky top-[100vh] bg-white text-black text-lg font-bold py-4 mt-8">
             <div className="container mx-auto px-4 flex justify-between items-center">
-                <p className="px-2 text-sm italic font-normal hover:text-slate-400">{currentYear} SWENG Group 12. All rights reserved.</p>
                 <Link to="/about">
                     Made by SWENG Group 12 in 🍀
                 </Link>
+                <p className="px-2 text-sm italic font-normal hover:text-slate-400">{currentYear} SWENG Group 12. All rights reserved.</p>
                 <p>
                     <Link onClick={(e) => handleClearCache(e)} to="/video" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><NavbarButton buttonText="Clear Local Data" /></Link>
                 </p>
-
             </div>
         </footer>
     );
--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import DashboardCard from "./Card";
+import { VideoContext } from "./VideoUtil";
+import { useContext } from "react";
 
 /**
  * 
@@ -12,17 +14,21 @@ import DashboardCard from "./Card";
 
 
 function LandingPage() {
+
+
+    const { token, logout } = useContext(VideoContext);
+
+
     return (
         <div className="min-h-screen">
             <Navbar />
             <div className="text-center">
-            <div className="text-center">
-                <h1 className="text-2xl font-bold text-center mb-8 mt-8 bg-white inline-block rounded py-2 px-4">Welcome to the user dashboard</h1>
-            </div>
-            <h2>Here you can see all the videos you've analysed along with all of our video analysis services...</h2>
+                <div className="text-center">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-8 bg-white inline-block rounded py-2 px-4">Welcome to your dashboard <p className="inline-block rotating-username-text">{token}</p>!</h1>
+                </div>
                 <div className="flex flex-row justify-between mx-auto py-4 px-4">
                     <DashboardCard
-    
+
                         cardTitle="Upload your own video"
                         cardDescription="Upload your own video to our machine learning powered algorithm for analysis! You will receive instantaneous analytics as to what objects your video contains in addition to locating objects within frames"
                         imgUrl="https://universalcaptions.files.wordpress.com/2016/05/upload-video.png?w=1200"
@@ -46,9 +52,8 @@ function LandingPage() {
                         buttonLink="/live"
                     />
                 
+                </div>
             </div>
-            </div>
-           
             <Footer/>
         </div>
     );
