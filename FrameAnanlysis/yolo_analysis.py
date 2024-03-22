d459ad96c31f150b029d7c451dcf895dc27a8a41
Ayushmaan04 <yadavay@tcd.ie>
2024-03-22 13:31:44 +0000

    updated

--- FrameAnanlysis/yolo_analysis.py
+++ FrameAnanlysis/yolo_analysis.py
@@ -3,10 +3,13 @@ frame = "FrameAnanlysis\\testImg.jpeg"
 model = YOLO('yolov8n.pt')
 
 def frameAnalysis(frame):
-    results = model(source= frame, show = False, conf = 0.4, save = True)
-
-
-    # View results
-    for r in results:
-        print(r.keypoints)
+    results = model(source= frame, show = False, conf = 0.4, save = True, save_txt	= True, show_labels = True)
+    # print(results)
+    for result in results:
+        if(result):
+            boxes = result[0].boxes.numpy()  # Boxes object for bbox outputs
+            for box in boxes:  # there could be more than one detection
+                print("class", box.cls)
+                print("xyxy", box.xyxy)
+                print("conf", box.conf)
 frameAnalysis(frame)
