0d0c07ede9259613b7df03e08e1f75ed10fb8d74
Finn Cummins <finnkcummins@gmail.com>
2024-03-25 22:51:56 +0000

    Altered test to test the new VideoCard component

--- frontend/test/upload.test.jsx
+++ frontend/test/upload.test.jsx
@@ -4,6 +4,7 @@ import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { VideoContext } from "../src/components/VideoUtil";
 import SingleVideoUpload from "../src/components/SingleVideoUpload";
+import VideoCard from "../src/components/VideoCard";
 import '@testing-library/jest-dom';
 import { MemoryRouter } from "react-router-dom";
 
@@ -25,6 +26,7 @@ describe('VideoUpload', () => {
         await user.click(screen.getByRole('button', {name: 'Submit'}));
     }); 
     it('changes when submitted', async () => {
+
         const {rerender} = render(
             <MemoryRouter>
             <VideoContext.Provider value={{videos : [{file:testFile, uploaded : true, analysed: false, name: "test.mp4"}], setVideos: () => {} }}>
@@ -35,20 +37,39 @@ describe('VideoUpload', () => {
         
         await waitFor(() => expect(screen.getByText("Analysing test.mp4 ...")).toBeInTheDocument(),{
         });
-        //screen.debug();
+
+        const dummyResult = {
+            name: "test.mp4",
+            results: [{
+                frame_number: 1,
+                results: [{ class_id: "test", conf: 0.62 }],
+                image: "base64imageString"
+            },
+            {
+                frame_number: 1,
+                results: [{ class_id: "test", conf: 0.54 }],
+                image: "base64imageString"
+            }]
+        };
+
         rerender(
             <MemoryRouter>
-            <VideoContext.Provider value={{videos : [{file:testFile, uploaded : true, analysed: true, name: "test.mp4"}], setVideos: () => {} }}>
-                    <SingleVideoUpload video={{file:testFile, uploaded : true, analysed: true, name: "test.mp4"}} />
-            </VideoContext.Provider>
+                <VideoContext.Provider value={{ 
+                    videos: [{ file: testFile, uploaded: true, analysed: true, name: "test.mp4", duration: "2:34" }], 
+                    setVideos: () => {}, 
+                    resultList: [dummyResult], 
+                    setResultList: () => {}
+                }}>
+                    <VideoCard result={dummyResult} video={{ name: "test.mp4", duration: "2:34" }} />
+                </VideoContext.Provider>
             </MemoryRouter>
         );
-        await waitFor(() => expect(screen.getByText("Click Here to View Analysis of test.mp4")).toBeInTheDocument(),{
-            timeout: 1500,
+
+        await waitFor(() => {
+            expect(screen.getByText('test.mp4')).toBeInTheDocument();
         });
-        //screen.debug();
-        });
-        
+    });
 }, {
     timeout: 1500,
-});
+}
+);
