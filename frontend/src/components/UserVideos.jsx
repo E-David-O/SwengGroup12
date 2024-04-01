433c4dda28f8005200182838a6656f8fee60381d
Louie Git <somersl@tcd.ie>
2024-04-01 22:03:11 +0100

    changes to user videos page

--- frontend/src/components/UserVideos.jsx
+++ frontend/src/components/UserVideos.jsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import VideoCard from "./VideoCard";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -6,12 +7,40 @@ import Footer from "./Footer";
 function UserVideos() {
     const [videos, setVideos] = useState([]);
     const username = JSON.parse(localStorage.getItem("username"));
+
+
+    const parseVideos = async (videos) => {
+        let newVideos = [];
+        for(let res of videos){
+            console.log(res);
+            if(res.encoded_video.includes("youtube.com") || res.encoded_video.includes("youtu.be") || res.encoded_video.includes("vimeo")){
+                let title;
+                await axios.get(`https://noembed.com/embed?dataType=json&url=${res.encoded_video}`)
+                .then(res => {
+                    console.log(res);
+                    title = res.data.title
+                })
+                .catch(err => console.log(err));
+                console.log(title);
+                newVideos.push({title: title, url: res.encoded_video});
+            } else if (res.encoded_video.includes("tiktok")) {
+                newVideos.push({title: res.encoded_video.split("/").slice(-1).toString(), url: res.encoded_video});
+            } else {
+                var blob = new Blob([atob(res.encoded_video)], { type: "video/mp4" });
+                newVideos.push({title: res.encoded_video, url: URL.createObjectURL(blob)});
+            }
+        }
+        return newVideos;
+    }
   useEffect(() => {
     axios
       .get(`http://localhost:8000/account_videos${(username) ? `?username=${username}` : ''}`)
       .then((response) => {
         console.log(response.data);
-        setVideos(response.data);
+        const newVideo = parseVideos(response.data);
+        console.log(newVideo);
+        setVideos([...newVideo]);
+
       })
       .catch((error) => {
         console.log(error);
@@ -23,9 +52,10 @@ function UserVideos() {
 
     <div className="min-h-screen">
         <Navbar />
-      {videos.map((video, i) => (
-        <p>{video.name}</p>
-      ))}
+      {videos.length !== 0 ? videos?.map((video, i) => (
+        <p>{video.encoded_video}</p>
+      ))
+        : <p>No videos uploaded</p>}
       <Footer />
     </div>
   );
