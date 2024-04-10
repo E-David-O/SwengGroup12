8a83568f4e6bc0d84a162747b806031302524a07
Daire Frankling <dairefrankling@Daires-MacBook-Air.local>
2024-04-10 12:07:38 +0100

    Added Animation to log in and Sign up pages

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -61,8 +61,23 @@ export default function Login() {
 
     return (
         <>
+            <style>{`
+                @keyframes backgroundChange {
+                    0% { background-color: rgba(16,24,39,1); }
+                    20% { background-color: rgba(16,24,39,0.8); }
+                    40% { background-color: rgba(16,24,39,0.6); }
+                    50% { background-color: rgba(16,24,39,0.5); }
+                    60% { background-color: rgba(16,24,39,0.6); }
+                    80% { background-color: rgba(16,24,39,0.8); }
+                    100% { background-color: rgba(16,24,39,1); }
+                }
+
+                .dynamic-background {
+                    animation: backgroundChange 10s ease-in-out infinite;
+                }
+            `}</style>
             <Navbar/>
-            <div className="bg-gray-50 dark:bg-gray-900">
+            <div className="bg-gray-50 dark:bg-gray-900 dynamic-background">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
